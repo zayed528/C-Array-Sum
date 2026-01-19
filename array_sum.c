f59c06db9ae974928da28f5d051f6df05c569f5c
ali.1189 <ali.1189@coe-dnc268474s.coeit.osu.edu>
2026-01-19 18:34:05 -0500

    Add array sum program

--- array_sum.c
+++ array_sum.c
@@ -4,5 +4,12 @@ int array_sum(int arr[], int size) {
     for (int i = 0; i < size; i++) {
         sum += arr[i];
     }
-    return sum;
+    fprintf(stdout, "%d\n", sum);
+    return sum;  
+}
+int main() {
+    int numbers[] = {1, 2, 3, 4, 5};
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+    array_sum(numbers, size);
+    return 0;
 }
